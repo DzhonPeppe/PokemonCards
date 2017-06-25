9424504bec81a9cbf095932156a0a96975387d30
Dzhon <john.peppe@mail.ru>
2017-06-25 06:52:04 +0300

    Alpha PokeCards ready for QA

---
+++ PokeCards/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  PokeCards
+//
+//  Created by John Peppe on 6/24/17.
+//  Copyright © 2017 John Peppe. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+
+typealias DownloadComplete = () -> ()
---
+++ PokeCards/PokeCell.swift
@@ -0,0 +1,35 @@
+//
+//  PokeCell.swift
+//  PokeCards
+//
+//  Created by John Peppe on 6/22/17.
+//  Copyright © 2017 John Peppe. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell (pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexID)")
+        
+        
+    }
+    
+}
